261e797e9d9409fb27c41bc62b78c95739a51d60
rektaka <rektaka@digitalspacedao.org>
2025-04-01 00:48:14 +0300

    Removed working hours section from Contact page

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -20,7 +20,7 @@ function Contact() {
     whatsapp: '+36 30 124 0003',
     email: 'osmautomec@gmail.com',
     phone: '+36 30 124 0003',
-    address: 'AUTOMEC Ipari Berendezések Kft., 1103 Budapest, Gyömrői út 115. Start Center Business Park/101',
+    address: 'AUTOMEC Ipari Berendezések Kft. Gyömrői út 115. Start Center Business Park/101 1103 Budapest,Hungary',
     mapUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11069.595180122315!2d19.143364121306266!3d47.46730663883507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c37bd223f27d%3A0xd06be78c858fd328!2sAutomec%20Ipari%20Berendezesek%20Kft.!5e1!3m2!1sen!2str!4v1742481809970!5m2!1sen!2str',
   };
 
@@ -107,21 +107,6 @@ function Contact() {
                       </Typography>
                     </Box>
                   </Stack>
-                </Box>
-
-                <Box>
-                  <Typography variant="h6" gutterBottom>
-                    Business Hours
-                  </Typography>
-                  <Typography>
-                    Monday - Friday: 9:00 AM - 6:00 PM
-                  </Typography>
-                  <Typography>
-                    Saturday: 10:00 AM - 2:00 PM
-                  </Typography>
-                  <Typography>
-                    Sunday: Closed
-                  </Typography>
                 </Box>
               </Stack>
             </Paper>
